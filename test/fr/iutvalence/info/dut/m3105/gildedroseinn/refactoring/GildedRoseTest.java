c1638005e60dfe7aa248f9cda48c7f2341943a3d
Anthony Gelibert <anthony.gelibert@me.com>
2015-11-07 00:21:39 +0100

    Step 3: RED

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -1,15 +1,42 @@
 package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
-import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
-public class GildedRoseTest
-{
-	@Test
-	public void aSimpleBeginningTest()
-	{
-		assertEquals(true,false);
-	}
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
+/**
+ * {@link GildedRose} test class.
+ *
+ * @author Anthony GELIBERT
+ * @version 1.0.0
+ */
+public final class GildedRoseTest {
+    /** Number of steps used to validate the update. */
+    private static final int NUMBER_OF_VALIDATION_STEPS = 5;
+
+    @Test
+    public void endOfDayWithoutItemTest() {
+        final List<Item> items = new ArrayList<>(0);
+        GildedRose.updateItems(items);
+        assertEquals(0, items.size());
+    }
+
+    @Test
+    public void endOfDayForSeveralItemsTest() {
+        final List<Item> items = new ArrayList<>(3);
+        items.add(new Item("a", 1, 2));
+        items.add(new Item("b", 1, 2));
+        items.add(new Item("c", 1, 2));
+
+        for (int day = 1; day <= NUMBER_OF_VALIDATION_STEPS; day++) {
+            GildedRose.updateItems(items);
+            for (final Item item : items) {
+                assertEquals(1 - day, item.getSellIn());
+                assertEquals(2 - day, item.getQuality());
+            }
+        }
+    }
 }

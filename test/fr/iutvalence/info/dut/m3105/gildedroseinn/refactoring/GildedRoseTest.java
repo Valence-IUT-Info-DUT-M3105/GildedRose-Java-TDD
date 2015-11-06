27201378822e292664251dab3f4ee0f0832440b5
Anthony Gelibert <anthony.gelibert@me.com>
2015-11-07 00:51:46 +0100

    STEP 4: RED

--- test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
+++ test/fr/iutvalence/info/dut/m3105/gildedroseinn/refactoring/GildedRoseTest.java
@@ -1,9 +1,10 @@
 package fr.iutvalence.info.dut.m3105.gildedroseinn.refactoring;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,28 +15,31 @@ import static org.junit.Assert.assertEquals;
  * @version 1.0.0
  */
 public final class GildedRoseTest {
-    /** Number of steps used to validate the update. */
-    private static final int NUMBER_OF_VALIDATION_STEPS = 5;
+    /** List of items used in each test. */
+    private Collection<Item> m_items;
+
+    @Before
+    public void setUp() throws Exception {
+        m_items = new LinkedList<>();
+    }
 
     @Test
     public void endOfDayWithoutItemTest() {
-        final Collection<Item> items = new ArrayList<>(0);
-        GildedRose.updateItems(items);
-        assertEquals(0, items.size());
+        GildedRose.updateItems(m_items);
+        assertEquals(0, m_items.size());
     }
 
     @Test
     public void endOfDayForSeveralItemsTest() {
-        final Collection<Item> items = new ArrayList<>(3);
-        items.add(new Item("a", 1, 2));
-        items.add(new Item("b", 1, 2));
-        items.add(new Item("c", 1, 2));
+        m_items.add(new Item("a", 5, 5));
+        m_items.add(new Item("b", 5, 5));
+        m_items.add(new Item("c", 5, 5));
 
-        for (int day = 1; day <= NUMBER_OF_VALIDATION_STEPS; day++) {
-            GildedRose.updateItems(items);
-            for (final Item item : items) {
-                assertEquals(1 - day, item.getSellIn());
-                assertEquals(2 - day, item.getQuality());
+        for (int day = 1; day <= 10; day++) {
+            GildedRose.updateItems(m_items);
+            for (final Item item : m_items) {
+                assertEquals(5 - day - ((day > 5) ? (day - 5) : 0), item.getQuality());
+                assertEquals(5 - day, item.getQuality());
             }
         }
     }
